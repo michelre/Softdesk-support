a9916cfffb23b9cf296c407b11ae70655c97bc25
Myriam Desporte <myriam.desporte@gmail.com>
2026-03-04 14:21:51 +0100

    Add User model with GDPR fields

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom user model with GDPR compliance fields.
+    """
+    date_of_birth = models.DateField(null=True, blank=True)
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def age(self):
+        """Calculate user's age from date of birth."""
+        if self.date_of_birth:
+            today = date.today()
+            birth_date = self.date_of_birth
+            return today.year - birth_date.year - (
+                    (today.month, today.day) < (birth_date.month, birth_date.day)
+            )
+        return None
