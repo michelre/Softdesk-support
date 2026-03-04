59d15586b312aa846c52e78ba39b300ec6a98110
Michel Rémi <remi.michel38@gmail.com>
2026-03-04 17:56:09 +0100

    feat(): Raise error if age < 15

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
 from .models import User
+from datetime import date
 
 
 class UserSerializer(serializers.ModelSerializer):
     """
     Serializer for User model.
     """
+
+    def create(self, validated_data):  
+        if validated_data.get('date_of_birth') is None:
+            raise serializers.ValidationError("Date of birth is required.")
+        
+        today = date.today()
+        birth_date = validated_data['date_of_birth']
+        age = today.year - birth_date.year - (
+                (today.month, today.day) < (birth_date.month, birth_date.day)
+        )            
+        if age < 15:
+            raise serializers.ValidationError("User must be at least 15 years old.")             
+
+        return User.objects.create_user(**validated_data)
 
     class Meta:
         model = User
